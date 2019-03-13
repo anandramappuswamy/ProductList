342b64ca3290524266e1e3b7700965f808c8fa29
anandramkpmg <anandram.appuswamy@kpmg.co.uk>
2019-03-13 11:30:45 +0000

    implemented detection changes changeDetector, this wont make any huge difference for now

--- src/app/Products/product-list-component/product-list-component.ts
+++ src/app/Products/product-list-component/product-list-component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 import { ProductService } from '../Service/product-service.service';
 import { Product } from '../Model/product';
 
 @Component({
   selector: 'app-product-list-component',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './product-list-component.html',
   styleUrls: ['./product-list-component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private changeDetector: ChangeDetectorRef) { }
 
-  records: Product[];
+  products: Product[];
   products$ = this.productService.data$;
 
   ngOnInit() {
     this.productService.loadProducts();
+    this.productService.data$.subscribe(data=>{
+      this.products = data;
+      this.changeDetector.markForCheck();
+    })
   }
 }
